725cdc8cf020f1e07dae91084f8b79972eb331aa
aloheston <alex@thedailybeast.com>
2026-04-01 14:35:53 -0400

    blah blah

--- tdb_transformation/models/staging/substack/stg__substack_post_growth.sql
+++ tdb_transformation/models/staging/substack/stg__substack_post_growth.sql
@@ -14,15 +14,15 @@ parsed as (
         snapshot_date,
 
         -- Subscription totals
-        CAST(data.subscribes.totals.subscribes          AS INT64) as subscribes,
-        CAST(data.subscribes.totals.monthly_subscribes  AS INT64) as monthly_subscribes,
-        CAST(data.subscribes.totals.annual_subscribes   AS INT64) as annual_subscribes,
-        CAST(data.subscribes.totals.free_trials         AS INT64) as free_trials,
-        CAST(data.subscribes.totals.founding_subscribes AS INT64) as founding_subscribes,
+        SAFE_CAST(JSON_VALUE(data, '$.subscribes.totals.subscribes')          AS INT64) as subscribes,
+        SAFE_CAST(JSON_VALUE(data, '$.subscribes.totals.monthly_subscribes')  AS INT64) as monthly_subscribes,
+        SAFE_CAST(JSON_VALUE(data, '$.subscribes.totals.annual_subscribes')   AS INT64) as annual_subscribes,
+        SAFE_CAST(JSON_VALUE(data, '$.subscribes.totals.free_trials')         AS INT64) as free_trials,
+        SAFE_CAST(JSON_VALUE(data, '$.subscribes.totals.founding_subscribes') AS INT64) as founding_subscribes,
 
         -- Signup and unsubscribe totals
-        CAST(data.signups.total                         AS INT64) as signups,
-        CAST(data.unsubscribes.total                    AS INT64) as unsubscribes
+        SAFE_CAST(JSON_VALUE(data, '$.signups.total')                         AS INT64) as signups,
+        SAFE_CAST(JSON_VALUE(data, '$.unsubscribes.total')                    AS INT64) as unsubscribes
 
     from source
 )
